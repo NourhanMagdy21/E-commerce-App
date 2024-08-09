7e310b10c2fcca2464dc66b5e448f3a952ed2dc8
NourhanMagdy21 <150091752+NourhanMagdy21@users.noreply.github.com>
2024-08-09 11:14:58 +0300

    FirstCommit

---
+++ lib/services/categories_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:ecommerce_app/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class CategoriesService {
+  Future<List<ProductModel>> getCategoriesProduct(
+      {required String categoryName}) async {
+    http.Response response = await http.get(
+        Uri.parse('https://fakestoreapi.com/products/category/$categoryName'));
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+
+      List<ProductModel> productsList = [];
+      for (int i = 0; i < data.length; i++) {
+        productsList.add(
+          ProductModel.fromJson(data[i]),
+        );
+      }
+      return productsList;
+    } else {
+      throw Exception(
+          'There is a problem with status code${response.statusCode}');
+    }
+  }
+}
---
+++ lib/services/get_all_products_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import '../models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class GetAllProductsService {
+  Future<List<ProductModel>> getAllProducts() async {
+    http.Response response =
+        await http.get(Uri.parse('https://fakestoreapi.com/products'));
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+
+      List<ProductModel> productsList = [];
+      for (int i = 0; i < data.length; i++) {
+        productsList.add(
+          ProductModel.fromJson(data[i]),
+        );
+      }
+      return productsList;
+    } else {
+      throw Exception(
+          'There is a problem with status code${response.statusCode}');
+    }
+  }
+}
